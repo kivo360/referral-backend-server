e8d8b09c83b2f8ff4ab3162b59e0665eae227d6e
Kevin Hill <kah.kevin.hill@gmail.com>
2018-07-28 17:18:48 -0700

    Nooooooooo

--- referral/app.py
+++ referral/app.py
@@ -4,6 +4,10 @@ from sanic.response import json
 from sanic_cors import CORS, cross_origin
 from sanic_limiter import Limiter, get_remote_address
 # from refer_file import add_user_with_filters, go_through_network
+from sanic.response import text
+from sanic.exceptions import NotFound, SanicException
+
+
 import sys
 
 app = Sanic(__name__)
@@ -11,9 +15,20 @@ CORS(app, automatic_options=True)
 
 limiter = Limiter(app, global_limits=['100 per hour', '40 per minute'])
 
+
+
 app.static('/', './referral/static')
 app.static('/', './referral/static/index.html')
 app.blueprint(user_bp)
+@app.exception(NotFound)
+async def ignore_404s(request, exception):
+	return text("Yep, I totally found the page: {}".format(request.url))
+
+
+@app.exception(SanicException)
+async def ignore_404s(request, exception):
+	return text("HAHAHAHAH: {}".format(request.url))
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=9000, debug=True)
